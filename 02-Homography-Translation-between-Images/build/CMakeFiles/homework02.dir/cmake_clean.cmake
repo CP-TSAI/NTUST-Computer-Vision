6c7c2ddb2b9dbd7ecf0e435ef54ab4aa39c5c6cf
Tsai, Chin-Po <ctsai@irobot.com>
2024-03-04 22:49:55 -0500

    renamed files

---
+++ 02-Homography-Translation-between-Images/build/CMakeFiles/homework02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework02.dir/homework02.cpp.o"
+  "homework02"
+  "homework02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
